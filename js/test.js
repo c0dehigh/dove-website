245f59c5b6042df7c6653313585525384e7893d6
Hamed <high_dev@outlook.com>
2022-05-11 21:45:22 +0430

    Final website

---
+++ js/test.js
@@ -0,0 +1,8 @@
+let firstName = "hamed";
+let lastName = "zibandeh";
+
+if (firstName === "hamed" && lastName === "zibandeh") {
+  console.log("admin here");
+} else {
+  console.log("user here");
+}
